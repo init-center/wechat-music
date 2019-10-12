39859599817232117bfb60b1d3f5026f9e4beb2d
Mexion <1095705440@qq.com>
2019-10-13 02:33:47 +0800

    Initial Commit

---
+++ miniprogram/app.js
@@ -0,0 +1,18 @@
+//app.js
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        //云开发环境ID
+        env: 'music-dev-pw2v0',
+        //记录访问小程序的用户到云开发控制台中
+        traceUser: true,
+      })
+    }
+    //全局属性定义
+    this.globalData = {}
+  }
+})
